c91b4b0321b9bfb3c5e8a44bf26087ca8c16642e
tso <tso@teknik.io>
2017-07-17 22:57:46 -0400

    handy slice functions

---
+++ slices.go
@@ -0,0 +1,20 @@
+package main
+
+func unique(slice []string) []string {
+	for i := len(slice) - 1; i >= 0; i-- {
+		s := slice[i]
+		j := sort.SearchStrings(slice, s)
+		if i != j {
+			slice = remove(slice, j)
+		}
+	}
+	return slice
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[0:i], slice[i+1:]...)
+}
+
+func insert(slice []string, s string, i int) []string {
+	return append(slice[:i], append([]string{s}, slice[i:]...)...)
+}
